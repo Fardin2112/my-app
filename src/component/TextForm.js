a08000274788812ef92d5c7ff31171aef0cac74e
Fardin2112 <fardinalam2112@gmail.com>
2023-10-08 16:24:11 +0530

    update

--- src/component/TextForm.js
+++ src/component/TextForm.js
@@ -8,16 +8,19 @@ export default function TextForm(props) {
   // setText = "oiainc" right way
    
   const handleUpclick =()=>{
+    // text.toLocalUppercase is built function for upper case
     let newtext = text.toLocaleUpperCase();
     // here i changed the value of text to uppercase on click 
     setText(newtext);
   }
-  const handleOnchange = (evnet) => {
-    // event handling -> update the value of text on change
-    setText(evnet.target.value);
+  const handleOnchange = (event) => {
+    // event(instead of event anything can be) handling update the value of text on change
+    setText(event.target.value);
   }
   return (
-        <div>
+    <>  
+        {/* mb-3  or my-3 used for spacing, this is used after className*/}
+        <div className="container">
              <h1>{props.heading}</h1>
              <div class="mb-3">
                <label for="myBox"  class="form-label">Example textarea</label>
@@ -25,5 +28,10 @@ export default function TextForm(props) {
              </div> 
              <button className="btn btn-primary" onClick={handleUpclick}>Convert to UpperCase</button>
          </div> 
+         <div className="container my-3">
+              <h1>Text summary</h1>
+              <p>{text.split(" ").length} words , {text.length} Characters</p>
+          </div>
+    </>     
   )
 }
